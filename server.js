893cfb34dd8af83c4321f1dc3a366d6c929d8147
Tifanny Rivera <trivera51580@gmail.com>
2021-09-17 20:30:53 -0600

    working

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // dependencies
 const express = require('express')
-const path = require('path')
-const notes = require('./db/db.json')
+// const path = require('path')
+const dataBase = require('./db/db.json')
 const fs = require('fs')
+const routes = require('./api/htmlroutes')
+const uuid = require('./helpers/uuid')
 
 // PORT
 const PORT = process.env.PORT || 3001
@@ -13,46 +15,14 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(express.static('public'));
+app.use(routes);
 
-app.get('/api/notes', function (req, res) {
-  return res.json(notes)
+require('./api/apiroute')
+require('./api/htmlroutes')
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`)
+  if (err) console.error(err)
 })
 
-app.post('/api/notes', function (req, res) {
-  const newNote = req.body
-  newNote['id'] = currentID + 1
-  currentID ++
-  console.log(newNote)
-  notes.push(newNote)
-
-  newNote()
-})
-
-app.delete('api/notes/:id', function (req, res) {
-  const id = req.params.id
-  const idLess = note.filter(function(less) {
-    return less.id < id
-  })
-  
-})
-// app.use('/api', api)
-// app.use(express.static('public'))
-
-// require('./routers/index')
-// require('./routers/htmlnotes')
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, './public/index.html'))
-// })
-
-// app.get('/notes', (req, res) => 
-// res.sendFile(path.join(__dirname, './public/index.html'))
-// )
-
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, './public/index.html'))
-// )
-
-// app.listen(PORT, () => {
-//   console.log(`Listening at http://localhost:${PORT}`)
-// })
