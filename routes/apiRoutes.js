721614a0cb78e07c1f8bc6cd2c24ed8224f22e54
Tifanny Rivera <trivera51580@gmail.com>
2021-12-05 17:40:09 -0700

    deployed to heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,21 @@
 const router = require('express').Router();
 const saveData = require('../db/data');
 
-router.get('/', function (req, res) {
+router.get('/api/notes', function (req, res) {
     saveData
         .retrieveNotes()
         .then(notes => res.json(notes))
         .catch(err => res.status(500).json(err));
 });
 
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     saveData
         .addNote(req.body)
         .then((note) => res.json(note))
         .catch(err => res.status(500).json(err));
 });
 
-router.delete('/notes/:id', function (req, res) {
+router.delete('/api/notes/:id', function (req, res) {
     saveData
         .deleteNote(req.params.id)
         .then(() => res.json({ ok: true }))
